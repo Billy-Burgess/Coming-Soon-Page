1b26d495c1e6fb85d1e69f83843449d0fe174512
B_Burgess <140908055+Billy-Burgess@users.noreply.github.com>
2023-12-22 13:36:07 +0000

    Challange completed.
    A good insight into setting up JS for the email warning. Also best practice coding mobile first then desktop.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const submit = document.querySelector(".submit");
-const input = document.querySelector("form-input");
-const error = document.querySelector(".error-txt");
-const errorIcon = document.querySelector(".error-icon");
+const submit = document.querySelector("button");
+const input = document.querySelector(".form-input");
+const error = document.querySelector(".error-txt-hidden");
+const errorIcon = document.querySelector(".error-icon-hidden");
 
 submit.addEventListener("click", validate);
 
@@ -22,3 +22,29 @@ function validate(e) {
 isEmail = (input) => {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input);
 };
+
+
+// const submit = document.querySelector(".submit");
+// const input = document.querySelector("input");
+// const error = document.querySelector(".error-txt");
+// const errorIcon = document.querySelector(".error-icon");
+
+// submit.addEventListener("click", validate);
+
+// function validate(e) {
+//     e.preventDefault();
+
+//     const inputValue = input.value.trim();
+
+//     if (!isEmail(inputValue)) {
+//         error.style.display = "block";
+//         errorIcon.style.display = "block";
+//     } else {
+//         error.style.display = "none";
+//         errorIcon.style.display = "none";
+//     }
+// }
+
+// isEmail = (input) => {
+//     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input);
+// };
